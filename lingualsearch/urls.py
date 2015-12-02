31339b36d628fd8e71844963afc383dc8b0df2b7
nitish <goelnitish@gmail.com>
2015-12-01 20:21:26 -0500

    gitignore fixes in progess + urls file updated

--- lingualsearch/urls.py
+++ lingualsearch/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^search$', 'lingualsearch.search', name='search'),
+    url(r'^search$', 'lingualsearch.views.search'),
 ]
