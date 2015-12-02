58d4c4f44b512e6f7dca9ab30453cf502250e86c
nitish <goelnitish@gmail.com>
2015-12-01 19:03:15 -0500

    First commit

---
+++ lingualsearch/local_settings.py
@@ -0,0 +1,14 @@
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lingualsearch',
+        'USER': 'nitish',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
