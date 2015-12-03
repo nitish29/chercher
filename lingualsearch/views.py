4ff6af86141c3d68a806e7305cedb4bc4b11893a
Archit Verma <archit017@gmail.com>
2015-12-03 16:00:54 -0500

    json data visible on ui

--- lingualsearch/views.py
+++ lingualsearch/views.py
@@ -14,13 +14,13 @@ def search(request):
 		#pdb.set_trace()
 		errors = []
 
-		#search_context = request.GET['q']
-		#search_context = search_context.strip()
-		#formatted_string = search_context.replace(",", " ")
-		#formatted_string = ' '.join(formatted_string.split())
-		#data = urllib.parse.urlencode({'text_en': formatted_string, 'wt': 'json', 'indent': 'true'})
-		#data = data.encode('utf-8')
-		#req = urllib.request.urlopen('http://uakk04319339.archit017.koding.io:8983/solr/project1/select?q=text_en%3Afootball&wt=json&indent=true', data)
+		# search_context = request.GET['q']
+		# search_context = search_context.strip()
+		# formatted_string = search_context.replace(",", " ")
+		# formatted_string = ' '.join(formatted_string.split())
+		# data = urllib.parse.urlencode({'text_en': formatted_string, 'wt': 'json', 'indent': 'true'})
+		# data = data.encode('utf-8')
+		# req = urllib.request.urlopen('http://uakk04319339.archit017.koding.io:8983/solr/project1/select?q=text_en%3Afootball&wt=json&indent=true', data)
 		req = urllib.request.urlopen('http://uakk04319339.archit017.koding.io:8983/solr/project1/select?q=text_en%3Afootball&wt=json&indent=true')
 		content = req.read()
 		decoded_json_content = json.loads(content.decode())
