cb248c8a1a3f4a861761d5924160b3be4f67a165
nitish <goelnitish@gmail.com>
2015-12-03 23:33:50 -0500

    views.py refactored

--- lingualsearch/views.py
+++ lingualsearch/views.py
@@ -11,26 +11,13 @@ def search(request):
 	
 	try:
 		
-		pdb.set_trace()
 		errors = []
-
 		search_context = request.GET['q']
-		search_context = search_context.strip()
-		formatted_string = search_context.replace(",", " ")
-		formatted_string = ' '.join(formatted_string.split())
-		formatted_string = 'text_en:' + formatted_string
-		data = urllib.parse.urlencode({'q': formatted_string, 'wt': 'json', 'indent': 'true'})
-		data = data.encode('utf-8')
-		req = urllib.request.urlopen('http://uakk04319339.archit017.koding.io:8983/solr/project1/select', data)
-		#req = urllib.request.urlopen('http://uakk04319339.archit017.koding.io:8983/solr/project1/select?q=text_en%3Afootball&wt=json&indent=true')
-		content = req.read()
-		decoded_json_content = json.loads(content.decode())
-		#print(decoded_json_content)
+
+		type = "search"
+		decoded_json_content = makeSolrCall(search_context,type)
 		json_response = decoded_json_content["response"]
 		feed_data = json_response["docs"]
-		#suggestions=getSuggestions()
-		
-
 		context = { "data" : feed_data }
 		
 
@@ -46,24 +33,39 @@ def search(request):
 	
 	#return render(request, "search.html", search_context)
 def getSuggestions(request):
-	#pdb.set_trace()
+	pdb.set_trace()
 	search_context = request.GET['s']
-	search_context = search_context.strip()
-	formatted_string = search_context.replace(",", " ")
-	formatted_string = ' '.join(formatted_string.split())
-	request_params = urllib.parse.urlencode({'q': formatted_string, 'wt': 'json', 'indent': 'true'})
-	request_params = request_params.encode('utf-8')
-	suggestion_fetch_request=urllib.request.urlopen('http://uakk04319339.archit017.koding.io:8983/solr/project1/suggest', request_params)
-	raw_json=suggestion_fetch_request.read()
-	decoded_json_content = json.loads(raw_json.decode())
+	type = "suggestion"
+	decoded_json_content = makeSolrCall(search_context,type)
 	suggestions_list= decoded_json_content['spellcheck']['suggestions'][1]['suggestion']
 	output='<ul id="live-search-results" class="clearfix">'
 	for i in suggestions_list:
 		output=output+'<li id="'+i+'" class="search-result faq" onclick="displaySuggestion(this.id)">'+i+'</li>'
 	output=output+'</ul>'
-	#return render("suggestions.html")
 	return HttpResponse(output)
 
 def getAuto():
 	pdb.set_trace()
 	return render("suggestions.html")
+
+def makeSolrCall(search_query,type):
+	
+	search_context = search_query.strip()
+	formatted_string = search_context.replace(",", " ")
+	formatted_string = ' '.join(formatted_string.split())
+
+	if type == "search":
+		formatted_string = 'text_en:' + formatted_string
+		request_params = urllib.parse.urlencode({'q': formatted_string, 'wt': 'json', 'indent': 'true'})
+		request_params = request_params.encode('utf-8')
+		req = urllib.request.urlopen('http://uakk04319339.archit017.koding.io:8983/solr/project1/select', request_params)
+
+	else:
+		request_params = urllib.parse.urlencode({'q': formatted_string, 'wt': 'json', 'indent': 'true'})
+		request_params = request_params.encode('utf-8')
+		req=urllib.request.urlopen('http://uakk04319339.archit017.koding.io:8983/solr/project1/suggest', request_params)
+
+	content = req.read()
+	decoded_json_content = json.loads(content.decode())
+	return decoded_json_content	
+
