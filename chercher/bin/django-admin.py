77eec1ceb18d690d2f526a0579ecd710e7284579
Mostly Cookies <ashwin.what@gmail.com>
2015-12-04 20:48:27 -0500

    Richie/Ashwin: Adding query boosting to our god damn queries. Now everything is awesome.

---
+++ chercher/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/swine/Projects/UB/InformationRetrieval/InfoRetrieval01/chercher/chercher/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
