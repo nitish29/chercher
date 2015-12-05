4e6b95c77002c4bdeae3907f289fafc9766b3e14
Archit Verma <archit017@gmail.com>
2015-12-04 21:46:11 -0500

    extra files removed

--- chercher/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/swine/Projects/UB/InformationRetrieval/InfoRetrieval01/chercher/chercher/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
